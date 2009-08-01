30659ab8f5f461dcda8e9669f82bc73db384b9e4
Jeremy Voorhis <jvoorhis@jeremy-voorhiss-macbook-2.local>
2009-08-01 16:01:25 -0700

    Removed begin block from example

--- examples/play_midi.rb
+++ examples/play_midi.rb
@@ -10,26 +10,22 @@ graph.connect_node_input(mus, 0, out, 0)
 graph.open
 graph.init
 graph.show
+graph.start
 
-begin
-  graph.start
-  dls = mus.audio_unit
-  dls.note_on(0, 60, 100)
-  sleep 0.4
-  dls.note_on(0, 64, 100)
-  sleep 0.4
-  dls.note_on(0, 67, 100)
-  sleep 0.4
-  dls.note_on(0, 72, 100)
-  sleep 2
-  dls.note_off(0, 60)
-  dls.note_off(0, 64)
-  dls.note_off(0, 67)
-  dls.note_off(0, 72)
-  sleep 1
-rescue => e
-  puts e.class.name, e.message, e.backtrace
-ensure
-  graph.stop
-  graph.dispose
-end
+dls = mus.audio_unit
+dls.note_on(0, 60, 100)
+sleep 0.4
+dls.note_on(0, 64, 100)
+sleep 0.4
+dls.note_on(0, 67, 100)
+sleep 0.4
+dls.note_on(0, 72, 100)
+sleep 2
+dls.note_off(0, 60)
+dls.note_off(0, 64)
+dls.note_off(0, 67)
+dls.note_off(0, 72)
+sleep 1
+
+graph.stop
+graph.dispose
